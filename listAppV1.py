7c643d3bf21d7ead8ad44c0aeff4c7c6a86e467e
diamondrodz <79866911+diamondrodz@users.noreply.github.com>
2021-03-09 11:20:57 -0700

    Add files via upload

---
+++ listAppV1.py
@@ -0,0 +1,31 @@
+"""
+Program goals:
+1. Add items to a list (ints)
+2. Offer the user a choic of actions
+3. Pull the values stored at specific indexes
+4. Covert input to INTs
+
+"""
+
+def mainProgram():
+    myList = []
+    print("Hello, there! Let's work with lists!")
+    print("Please choose from the following options. Type the number of the choice")
+    choice = input("1. Add to list, 2. Return a value at a list    ")
+    if chocie == "1":
+        addToList()
+    elif choice == "2":
+        indexValues()
+
+def addToList():
+    print("Adding to a list! Great choice!")
+    newItem = input("Type an integer here!   ")
+    myList.append(int(newItem)
+    #we need to think about errors!
+
+def indexValues():
+    
+
+
+if _name_ == "_main_":
+    mainProgram()
